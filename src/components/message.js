6df6ed73498a9935374d3840109b55c70653d564
Thomas Tuts <thomas@thomastuts.com>
2015-12-15 18:21:13 +0100

    Use a stateless function component to display a message

--- src/components/message.js
+++ src/components/message.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-export default class Message extends React.Component {
-  render() {
-    return (
-      <div className={`message ${this.props.sender}`}>
-        {this.props.content}
-      </div>
-    );
-  }
-}
+const Message = ({sender, content}) => (
+  <div className={`message ${sender}`}>
+    {content}
+  </div>
+);
 
 Message.propTypes = {
   sender: React.PropTypes.string.isRequired,
   content: React.PropTypes.string.isRequired,
 };
+
+export default Message;
